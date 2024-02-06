6c3779a9c761d250f25ced4ab0196edbe2cfc9db
Fabine Specian <fabine.specian@gmail.com>
2024-02-06 20:51:50 +0000

    funcionalidade do filtro implementada.

--- src/view.js
+++ src/view.js
@@ -4,7 +4,7 @@ export const renderItems = (data) => {
 
   data.forEach((item) => {
     itens.innerHTML += `
- <div itemscope itemtype="carreirasTech" class="container_itens">
+ <li itemscope itemtype="carreirasTech" class="container_itens">
     <dl itemscope itemtype="carreirasTech"> 
       <dt></dt><dd itemprop="imageUrl"><img src="${item.imageUrl}" itemprop="${item.imageUrl}" alt="Profissional mulher da área da tecnologia." /> </dd>
       <dt></dt> <dd itemprop="name">${item.name}</dd>
@@ -12,7 +12,7 @@ export const renderItems = (data) => {
       <dt>Formação Sugerida:</dt> <dd itemprop="formacaoSugerida">${item.facts.formacaoSugerida}</dd>
       <dt>Tech Skills:</dt> <dd itemprop="techSkills">${item.facts.techSkills}</dd>
       <dt>Soft Skills:</dt> <dd itemprop="softSkills">${item.facts.softSkills}</dd>
-      <dt>Média Salarial Júnior:</dt> <dd itemprop="mediaSalarialJr">R$ ${item.facts.mediaSalarialJr}</dd>
+      <dt>Média Salarial Júnior:</dt> <dd itemprop="sort-order">R$ ${item.facts.mediaSalarialJr}</dd>
     </dl>
   </div>
  `;
