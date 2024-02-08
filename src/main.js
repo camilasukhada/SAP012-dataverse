e3ca669423fffe2bff7fe3a051338d03440c8406
Fabine Specian <fabine.specian@gmail.com>
2024-02-08 18:01:55 +0000

    retirada da class no select do html

--- src/main.js
+++ src/main.js
@@ -1,17 +1,28 @@
-import { filterData, sortData, computeStats } from './dataFunctions.js';
-import { renderItems, renderStatsMessage } from './view.js';
+import { filterData, sortData} from './dataFunctions.js';
+import { renderItems } from './view.js';
+
 import data from './data/dataset.js';
 
+
 const cardsItens = document.querySelector("#root");
+cardsItens.appendChild(renderItems(data))
+
+
 const filtro = document.querySelector("#filtro");
 
 let cardData = [...data];
 
 
 filtro.addEventListener("change", (e) => {
+  //recebe o valor do campo filtro de index.html
   let value = e.target.value;
+
+  //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a primeira posição do array que se refere ao campo que será o filtro
   const filterBy = value.split("|")[0];
+
+  //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a segunda  posição do array que se refere ao valor que irá buscar no dataset
   value = value.split("|")[1];
+
   if (value === "todos") {
     cardData = [...data];
   } else {
@@ -20,19 +31,7 @@ filtro.addEventListener("change", (e) => {
   cardsItens.innerHTML = "";
 
   cardsItens.appendChild(renderItems(cardData));
-
-  cardsItens.innerHTML = ""; // Limpa os itens anteriores
-
-  // Renderiza a mensagem de estatísticas apenas se uma categoria do filtro for selecionada
-  if (value !== "todos") {
-    const count = computeStats(data, filterBy, value);
-    const statsMessage = renderStatsMessage(count);
-    cardsItens.appendChild(statsMessage);
-  }
-
-  cardsItens.appendChild(renderItems(cardData)); // Renderiza os itens filtrados ou todos os itens
-});
-
+})
 
 const ordenacao = document.querySelector("#ordenacao");
 
@@ -42,16 +41,10 @@ ordenacao.addEventListener("change", (e) => {
   if (orderValue === "asc") {
     cardData = sortData(cardData, "mediaSalarialJr", "asc");
   }
-  else if (orderValue === "desc") {
+  else if (orderValue === "desc"){
     cardData = sortData(cardData, "mediaSalarialJr", "desc");
   }
 
   cardsItens.innerHTML = "";
   cardsItens.appendChild(renderItems(cardData));
-
-
-
-
-});
-
-cardsItens.appendChild(renderItems(data)); 
+})
