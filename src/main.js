6c03ee5e901a513bf86f4314dcccca6b68e1a579
camilasukhada <camilasukhada@gmail.com>
2024-02-08 11:27:46 -0300

    Função estatistica implementada

--- src/main.js
+++ src/main.js
@@ -1,37 +1,37 @@
-import { filterData } from './dataFunctions.js';
-import { renderItems } from './view.js';
-
+import { filterData, computeStats } from './dataFunctions.js';
+import { renderItems, renderStatsMessage } from './view.js';
 import data from './data/dataset.js';
 
-
 const cardsItens = document.querySelector("#root");
-cardsItens.appendChild(renderItems(data))
-
-
 const filtro = document.querySelector("#filtro");
 
 let cardData = [...data];
 
-
 filtro.addEventListener("change", (e) => {    
-    //recebe o valor do campo filtro de index.html
-    let value = e.target.value;
-    
-    //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a primeira posição do array que se refere ao campo que será o filtro
-    const filterBy = value.split("|")[0];
+  let value = e.target.value;
+  const filterBy = value.split("|")[0];
+  value = value.split("|")[1];
 
-    //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a segunda  posição do array que se refere ao valor que irá buscar no dataset
-    value = value.split("|")[1];
+  if (value === "todos") {
+    cardData = [...data];
+  } else {
+    cardData = filterData(data, filterBy, value);
+  }
 
-    if (value === "todos") {
-      cardData = [...data];
-    } else {
-      cardData = filterData(data, filterBy, value);
-    }
-    cardsItens.innerHTML = "";
-  
-    cardsItens.appendChild(renderItems(cardData));
-  })
+  cardsItens.innerHTML = ""; // Limpa os itens anteriores
+
+  // Renderiza a mensagem de estatísticas apenas se uma categoria do filtro for selecionada
+  if (value !== "todos") {
+    const count = computeStats(data, filterBy, value);
+    const statsMessage = renderStatsMessage(count);
+    cardsItens.appendChild(statsMessage);
+  }
+
+  cardsItens.appendChild(renderItems(cardData)); // Renderiza os itens filtrados ou todos os itens
+});
+
+cardsItens.appendChild(renderItems(data));
+
 
 
 
