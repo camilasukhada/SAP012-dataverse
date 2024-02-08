80a224af849d1649934e2991c80c8c9456ccddb1
Fabine Specian <fabine.specian@gmail.com>
2024-02-08 18:08:33 +0000

    Alteração da font-size no css

--- src/main.js
+++ src/main.js
@@ -1,29 +1,18 @@
-import { filterData, sortData} from './dataFunctions.js';
-import { renderItems } from './view.js';
-
+import { filterData, sortData, computeStats} from './dataFunctions.js';
+import { renderItems,renderStatsMessage } from './view.js';
 import data from './data/dataset.js';
 
-
 const cardsItens = document.querySelector("#root");
-cardsItens.appendChild(renderItems(data))
-
-
 const filtro = document.querySelector("#filtro");
 
 let cardData = [...data];
 
 
-filtro.addEventListener("change", (e) => {
-  //recebe o valor do campo filtro de index.html
+filtro.addEventListener("change", (e) => {    
   let value = e.target.value;
-
-  //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a primeira posição do array que se refere ao campo que será o filtro
   const filterBy = value.split("|")[0];
-
-  //usa o valor de "let value" para fazer o split do valor pelo caracter | e pega a segunda  posição do array que se refere ao valor que irá buscar no dataset
   value = value.split("|")[1];
-
-  if (value === "todos") {
+ if (value === "todos") {
     cardData = [...data];
   } else {
     cardData = filterData(data, filterBy, value);
@@ -31,7 +20,19 @@ filtro.addEventListener("change", (e) => {
   cardsItens.innerHTML = "";
 
   cardsItens.appendChild(renderItems(cardData));
-})
+
+  cardsItens.innerHTML = ""; // Limpa os itens anteriores
+
+  // Renderiza a mensagem de estatísticas apenas se uma categoria do filtro for selecionada
+  if (value !== "todos") {
+    const count = computeStats(data, filterBy, value);
+    const statsMessage = renderStatsMessage(count);
+    cardsItens.appendChild(statsMessage);
+  }
+
+  cardsItens.appendChild(renderItems(cardData)); // Renderiza os itens filtrados ou todos os itens
+});
+
 
 const ordenacao = document.querySelector("#ordenacao");
 
@@ -47,4 +48,10 @@ ordenacao.addEventListener("change", (e) => {
 
   cardsItens.innerHTML = "";
   cardsItens.appendChild(renderItems(cardData));
-})
+
+
+
+  
+});
+
+cardsItens.appendChild(renderItems(data));
