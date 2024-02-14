662d23a7c2b0c6831ed7f3e58a80d9ed163ceff6
Fabine Specian <fabine.specian@gmail.com>
2024-02-14 15:14:39 +0000

    Correção do erro de  identação do main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { filterData, sortData, computeStats} from './dataFunctions.js';
-import { renderItems,renderStatsMessage } from './view.js';
+import { filterData, sortData, computeStats } from './dataFunctions.js';
+import { renderItems, renderStatsMessage } from './view.js';
 import data from './data/dataset.js';
 
 const cardsItens = document.querySelector("#root");
@@ -8,11 +8,11 @@ const filtro = document.querySelector("#filtro");
 let cardData = [...data];
 
 
-filtro.addEventListener("change", (e) => {    
+filtro.addEventListener("change", (e) => {
   let value = e.target.value;
   const filterBy = value.split("|")[0];
   value = value.split("|")[1];
- if (value === "todos") {
+  if (value === "todos") {
     cardData = [...data];
   } else {
     cardData = filterData(data, filterBy, value);
@@ -42,7 +42,7 @@ ordenacao.addEventListener("change", (e) => {
   if (orderValue === "asc") {
     cardData = sortData(cardData, "mediaSalarialJr", "asc");
   }
-  else if (orderValue === "desc"){
+  else if (orderValue === "desc") {
     cardData = sortData(cardData, "mediaSalarialJr", "desc");
   }
 
@@ -50,7 +50,7 @@ ordenacao.addEventListener("change", (e) => {
   cardsItens.appendChild(renderItems(cardData));
 });
 
-cardsItens.appendChild(renderItems(data)); 
+cardsItens.appendChild(renderItems(data));
 
 const botaoLimpar = document.querySelector("#reset-button");
 botaoLimpar.addEventListener("click", () => {
